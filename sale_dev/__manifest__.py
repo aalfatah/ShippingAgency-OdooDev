49d18b4c7983f7e44650123e7d800e3687ceb672
BSS <hypur71@gmail.com>
2024-11-27 05:53:39 +0700

    Set invoice dp automatically from sales create invoice

---
+++ sale_dev/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Sale Dev",
+    "version": "16.0.0.0.0",
+    "category": "Sales/Sales",
+    "author": "Dev Team,",
+    "website": "https://dev.com",
+    "license": "LGPL-3",
+    "depends": ["sale", "account_dev"],
+    "data": [
+    ],
+    "installable": True,
+}
