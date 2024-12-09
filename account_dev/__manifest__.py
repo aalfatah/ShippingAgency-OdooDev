047918fb02e2625111fc42acf77913183fa00fc8
hypur71@gmail.com <08Dec1971>
2024-12-09 08:51:47 +0700

    Account modul update code

--- account_dev/__manifest__.py
+++ account_dev/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name' : 'Invoicing Dev',
-    'version' : '16.0',
+    'name': 'Invoicing Dev',
+    'version': '16.0',
     'summary': 'Invoices & Payments',
     'sequence': 10,
     'description': """
@@ -11,7 +11,7 @@ Invoicing & Payments
 Modify journal view show secure_sequence_id field
     """,
     'category': 'Accounting/Accounting',
-    'depends' : ['account'],
+    'depends': ['account'],
     'data': [
         'views/account_move_views.xml',
     ],
