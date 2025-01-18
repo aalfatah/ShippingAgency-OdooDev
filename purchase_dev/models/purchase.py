3e1de106c510017fa92cfc12682e6bc970a9ca07
hypur71@gmail.com <08Dec1971>
2025-01-18 16:25:18 +0700

    Nomer PO kembali ke format normal

--- purchase_dev/models/purchase.py
+++ purchase_dev/models/purchase.py
@@ -6,21 +6,21 @@ class PurchaseOrder(models.Model):
 
     partner_id = fields.Many2one('res.partner', domain="[('supplier_rank', '!=', 0)]")
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        orders = super(PurchaseOrder, self).create(vals_list)
-        for order in orders:
-            if '{VND}' in order.name:
-                order.update({
-                    'name': order.name.replace('{VND}', order.partner_id.name)
-                })
-        return orders
-
-    def write(self, vals):
-        partner_name = ''
-        if 'partner_id' in vals:
-            partner_name = self.partner_id.name
-        res = super().write(vals)
-        if 'partner_id' in vals:
-            self.name = self.name.replace(partner_name, self.partner_id.name)
-        return res
+    # @api.model_create_multi
+    # def create(self, vals_list):
+    #     orders = super(PurchaseOrder, self).create(vals_list)
+    #     for order in orders:
+    #         if '{VND}' in order.name:
+    #             order.update({
+    #                 'name': order.name.replace('{VND}', order.partner_id.name)
+    #             })
+    #     return orders
+    #
+    # def write(self, vals):
+    #     partner_name = ''
+    #     if 'partner_id' in vals:
+    #         partner_name = self.partner_id.name
+    #     res = super().write(vals)
+    #     if 'partner_id' in vals:
+    #         self.name = self.name.replace(partner_name, self.partner_id.name)
+    #     return res
