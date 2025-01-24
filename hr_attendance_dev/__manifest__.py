f58206295d2042d96eabab6c63ce7d464fdd203b
hypur71@gmail.com <08Dec1971>
2025-01-24 09:13:22 +0700

    Attendance filter this month and group by employee

---
+++ hr_attendance_dev/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Attendances Dev",
+    'version': '16.0.1.0.0',
+    "summary": "HR",
+    "category": "HR",
+    "author": "Dev Team",
+    "license": "",
+    "installable": True,
+    "maintainers": ["Dev Team"],
+    "depends": [
+        "hr_attendance",
+    ],
+    "data": [
+        "views/hr_attendance_view.xml",
+    ],
+    "sequence": 1,
+    "license": "LGPL-3",
+}
