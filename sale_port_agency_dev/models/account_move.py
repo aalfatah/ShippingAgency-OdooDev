e99afedc670a862c2f32a99ec3c36eee3aa9d79e
hypur71@gmail.com <08Dec1971>
2025-04-22 06:02:30 +0700

    Customer invoice add No. B/L

--- sale_port_agency_dev/models/account_move.py
+++ sale_port_agency_dev/models/account_move.py
@@ -12,6 +12,7 @@ class AccountMove(models.Model):
     sale_id = fields.Many2one('sale.order', 'Sales Order', compute='_get_sale_order')
     po_no = fields.Char(string='No. PO', compute='_get_sale_order')
     vo_no = fields.Char(string='No. VO', compute='_get_sale_order')
+    no_bl = fields.Char(string="No. B/L", compute='_get_sale_order')
     vessel_ids = fields.Many2many('agency.vessel', 'sale_order_vessel_rel', string='Vessel', compute='_get_sale_order')
     vessel_name = fields.Char(string='Vessel Name', related='sale_id.vessel_name')
     last_port_id = fields.Many2one('agency.port', string='Last Port', compute='_get_sale_order')
@@ -25,6 +26,7 @@ class AccountMove(models.Model):
             sale_id = False
             po_no = False
             vo_no = False
+            no_bl = False
             vessel_ids = False
             last_port_id = False
             load_port_ids = False
@@ -35,6 +37,7 @@ class AccountMove(models.Model):
                 sale_id = sale.id
                 po_no = sale.client_order_ref
                 vo_no = sale.vo_no
+                no_bl = sale.no_bl
                 vessel_ids = sale.vessel_ids
                 last_port_id = sale.last_port_id.id
                 load_port_ids = sale.load_port_ids
@@ -42,6 +45,7 @@ class AccountMove(models.Model):
             move.sale_id = sale_id
             move.po_no = po_no
             move.vo_no = vo_no
+            move.no_bl = no_bl
             move.vessel_ids = vessel_ids
             move.last_port_id = last_port_id
             move.load_port_ids = load_port_ids
