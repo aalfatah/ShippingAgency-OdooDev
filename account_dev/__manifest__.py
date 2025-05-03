f220d130ed098171b5ffcc06200f63715513d683
hypur71@gmail.com <08Dec1971>
2025-05-03 08:44:33 +0700

    Payment tree view show reference

--- account_dev/__manifest__.py
+++ account_dev/__manifest__.py
@@ -14,6 +14,7 @@ Modify journal view show secure_sequence_id field
     'depends': ['account', 'account_menu_invoice_refund', 'partner_manual_rank'],
     'data': [
         'views/account_move_views.xml',
+        'views/account_payment_view.xml',
         'reports/print_payment_voucher.xml',
         'reports/report_account_move.xml',
         'reports/report_invoice.xml',
