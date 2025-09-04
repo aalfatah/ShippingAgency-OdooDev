44d9d96b17b9ae735e73ec8a1c911b5a31cbdf7d
hypur71@gmail.com <08Dec1971>
2025-09-05 06:26:34 +0700

    Remove attachment on report

--- account_dev/__manifest__.py
+++ account_dev/__manifest__.py
@@ -18,6 +18,7 @@ Modify journal view show secure_sequence_id field
         'reports/print_payment_voucher.xml',
         'reports/report_account_move.xml',
         'reports/report_invoice.xml',
+        'reports/account_report.xml',
     ],
     'demo': [
     ],
